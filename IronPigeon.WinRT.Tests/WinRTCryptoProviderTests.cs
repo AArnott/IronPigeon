2d456b538698565786d586e5f9a1f99273287a52
Andrew Arnott <andrewarnott@gmail.com>
2012-07-28 09:38:28 -0700

    Added WinRT unit tests.

---
+++ IronPigeon.WinRT.Tests/WinRTCryptoProviderTests.cs
@@ -0,0 +1,15 @@
+﻿namespace IronPigeon.WinRT.Tests {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+	[TestClass]
+	public class WinRTCryptoProviderTests {
+		[TestMethod]
+		public void Ctor() {
+			var provider = new WinRTCryptoProvider();
+		}
+	}
+}
