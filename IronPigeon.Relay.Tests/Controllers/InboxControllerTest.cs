42f48930475f3a5d63109d6e701a313d833dcadf
Andrew Arnott <andrewarnott@gmail.com>
2012-08-09 21:06:45 -0700

    Made MVC test more precise.

--- IronPigeon.Relay.Tests/Controllers/InboxControllerTest.cs
+++ IronPigeon.Relay.Tests/Controllers/InboxControllerTest.cs
@@ -13,7 +13,12 @@
 			var controller = new InboxController();
 			string thumbprint = null;
 			ActionResult result = controller.List(thumbprint).Result;
-			Assert.That(result, Is.Not.Null);
+
+			Assert.That(result, Is.InstanceOf<JsonResult>());
+			var jsonResult = (JsonResult)result;
+			Assert.That(jsonResult.JsonRequestBehavior, Is.EqualTo(JsonRequestBehavior.AllowGet));
+			var data = (IncomingList)jsonResult.Data;
+			Assert.That(data, Is.Not.Null);
 		}
 
 		[Test, Ignore("Not yet implemented")]
