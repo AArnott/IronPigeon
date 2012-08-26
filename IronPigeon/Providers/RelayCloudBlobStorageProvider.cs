8a26d6ff3187bbf27f1c6953dd192919c465c2d9
Andrew Arnott <andrewarnott@gmail.com>
2012-08-26 15:44:47 -0700

    RelayCloudBlobStorageProvider now has public property.

--- IronPigeon/Providers/RelayCloudBlobStorageProvider.cs
+++ IronPigeon/Providers/RelayCloudBlobStorageProvider.cs
@@ -13,9 +13,13 @@
 	using Microsoft;
 
 	public class RelayCloudBlobStorageProvider : ICloudBlobStorageProvider {
-		private readonly Uri postUrl;
-
 		private HttpMessageHandler httpMessageHandler = new HttpClientHandler();
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RelayCloudBlobStorageProvider" /> class.
+		/// </summary>
+		public RelayCloudBlobStorageProvider() {
+		}
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="RelayCloudBlobStorageProvider" /> class.
@@ -23,7 +27,7 @@
 		/// <param name="postUrl">The URL to post blobs to.</param>
 		public RelayCloudBlobStorageProvider(Uri postUrl) {
 			Requires.NotNull(postUrl, "postUrl");
-			this.postUrl = postUrl;
+			this.PostUrl = postUrl;
 			this.HttpClient = new HttpClient(this.httpMessageHandler);
 		}
 
@@ -43,6 +47,11 @@
 		}
 
 		/// <summary>
+		/// Gets or sets the URL to post blobs to.
+		/// </summary>
+		public Uri PostUrl { get; set; }
+
+		/// <summary>
 		/// Gets the HTTP client to use for outbound HTTP requests.
 		/// </summary>
 		protected HttpClient HttpClient { get; private set; }
@@ -50,7 +59,7 @@
 		public async Task<Uri> UploadMessageAsync(Stream content, DateTime expirationUtc, string contentType = null, string contentEncoding = null, CancellationToken cancellationToken = default(CancellationToken)) {
 			var httpContent = new StreamContent(content);
 			httpContent.Headers.ContentType = new MediaTypeHeaderValue("image/png");
-			var response = await this.HttpClient.PostAsync(this.postUrl.AbsoluteUri + "?lifetimeInMinutes=5", httpContent);
+			var response = await this.HttpClient.PostAsync(this.PostUrl.AbsoluteUri + "?lifetimeInMinutes=5", httpContent);
 			response.EnsureSuccessStatusCode();
 
 			var serializer = new DataContractJsonSerializer(typeof(string));
