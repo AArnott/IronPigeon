ce87074fa9375ec17d81250ce7cda0193816c83c
Andrew Arnott <andrewarnott@gmail.com>
2012-08-30 08:39:21 -0700

    Added a U.S. exportable crypto security level.
    The U.S. limits software exports that use encryption to ridiculously low
    strengths.
    This change defines that maximum allowable strength as a new low in
    SecurityLevel so that apps can ship using that level without worrying
    about U.S. registration.
    http://www.bis.doc.gov/encryption/question2.htm

--- IronPigeon/SecurityLevel.cs
+++ IronPigeon/SecurityLevel.cs
@@ -15,13 +15,23 @@
 		/// The recommended security level for personal or business communications.
 		/// Keys may take several seconds to generate, but provide maximum protection.
 		/// </summary>
-		public static readonly SecurityLevel Recommended = new MaximumLevel();
+		public static readonly SecurityLevel Maximum = new MaximumLevel();
+
+		/// <summary>
+		/// The maximum security level allowed by the Bureau of Industry and Security, U.S. Department of Commerce
+		/// without a requirement to file an encryption registration.
+		/// This is actually even less security than <see cref="Minimum"/>.
+		/// </summary>
+		/// <remarks>
+		/// See http://www.bis.doc.gov/encryption/question2.htm for details.
+		/// </remarks>
+		public static readonly SecurityLevel UnitedStatesExportableMaximum = new UnitedStatesExportableMaximumLevel();
 
 		/// <summary>
 		/// The minimal security level that still includes encryption and signatures.
 		/// Keys are generated much faster, but provide less protection.
 		/// </summary>
-		public static readonly SecurityLevel Minimal = new MinimalLevel();
+		public static readonly SecurityLevel Minimum = new MinimumLevel();
 
 		/// <summary>
 		/// Gets the name of the hash algorithm.
@@ -30,6 +40,11 @@
 		/// The name of the hash algorithm.
 		/// </value>
 		public abstract string HashAlgorithmName { get; }
+
+		/// <summary>
+		/// Gets the name of the symmetric algorithm to use.
+		/// </summary>
+		public abstract string SymmetricAlgorithmName { get; }
 
 		/// <summary>
 		/// Gets the size of the encryption asymmetric key.
@@ -63,6 +78,7 @@
 			Requires.NotNull(cryptoProvider, "cryptoProvider");
 
 			cryptoProvider.HashAlgorithmName = this.HashAlgorithmName;
+			cryptoProvider.SymmetricAlgorithmName = this.SymmetricAlgorithmName;
 			cryptoProvider.EncryptionAsymmetricKeySize = this.EncryptionAsymmetricKeySize;
 			cryptoProvider.SignatureAsymmetricKeySize = this.SignatureAsymmetricKeySize;
 			cryptoProvider.BlobSymmetricKeySize = this.BlobSymmetricKeySize;
@@ -71,7 +87,7 @@
 		/// <summary>
 		/// Minimal key sizes.
 		/// </summary>
-		private class MinimalLevel : SecurityLevel {
+		private class MinimumLevel : SecurityLevel {
 			/// <summary>
 			/// Gets the name of the hash algorithm.
 			/// </summary>
@@ -80,6 +96,13 @@
 			/// </value>
 			public override string HashAlgorithmName {
 				get { return "SHA1"; }
+			}
+
+			/// <summary>
+			/// Gets the name of the symmetric algorithm to use.
+			/// </summary>
+			public override string SymmetricAlgorithmName {
+				get { return "Rijndael"; }
 			}
 
 			/// <summary>
@@ -114,6 +137,60 @@
 		}
 
 		/// <summary>
+		/// The maximum security level allowed by the Bureau of Industry and Security, U.S. Department of Commerce
+		/// without a requirement to file an encryption registration.
+		/// This is actually quite minimal strength encryption.
+		/// </summary>
+		private class UnitedStatesExportableMaximumLevel : SecurityLevel {
+			/// <summary>
+			/// Gets the name of the hash algorithm.
+			/// </summary>
+			/// <value>
+			/// The name of the hash algorithm.
+			/// </value>
+			public override string HashAlgorithmName {
+				get { return "SHA1"; }
+			}
+
+			/// <summary>
+			/// Gets the name of the symmetric algorithm to use.
+			/// </summary>
+			public override string SymmetricAlgorithmName {
+				get { return "RC2"; } // the only symmetric algorithm in .NET that allows less than 64-bit key sizes
+			}
+
+			/// <summary>
+			/// Gets the size of the encryption asymmetric key.
+			/// </summary>
+			/// <value>
+			/// The size of the encryption asymmetric key.
+			/// </value>
+			public override int EncryptionAsymmetricKeySize {
+				get { return 512; }
+			}
+
+			/// <summary>
+			/// Gets the size of the signature asymmetric key.
+			/// </summary>
+			/// <value>
+			/// The size of the signature asymmetric key.
+			/// </value>
+			public override int SignatureAsymmetricKeySize {
+				get { return 512; }
+			}
+
+			/// <summary>
+			/// Gets the size of the BLOB symmetric key.
+			/// </summary>
+			/// <value>
+			/// The size of the BLOB symmetric key.
+			/// </value>
+			public override int BlobSymmetricKeySize {
+				get { return 56; }
+			}
+		}
+
+		/// <summary>
 		/// Maximum recommended key length.s.
 		/// </summary>
 		private class MaximumLevel : SecurityLevel {
@@ -125,6 +202,13 @@
 			/// </value>
 			public override string HashAlgorithmName {
 				get { return "SHA256"; }
+			}
+
+			/// <summary>
+			/// Gets the name of the symmetric algorithm to use.
+			/// </summary>
+			public override string SymmetricAlgorithmName {
+				get { return "Rijndael"; }
 			}
 
 			/// <summary>
