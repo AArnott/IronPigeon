a1b35efb266113a195b9749c8ffb61060bbd7189
Andrew Arnott <andrewarnott@gmail.com>
2012-09-15 12:40:00 -0700

    Added home page to message relay service.

---
+++ src/IronPigeon.Relay/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿namespace IronPigeon.Relay.Controllers {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Web;
+	using System.Web.Mvc;
+
+	public class HomeController : Controller {
+		public ActionResult Index() {
+			return View();
+		}
+	}
+}
