36c0b23edf207a9598da7ec3f2f5c18cc5930067
Andrew Arnott <andrewarnott@gmail.com>
2012-10-13 16:26:51 -0700

    Added privacy policy.

---
+++ src/IronPigeon.Relay/Controllers/PrivacyController.cs
@@ -0,0 +1,16 @@
+﻿namespace IronPigeon.Relay {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Web;
+	using System.Web.Mvc;
+
+	public class PrivacyController : Controller {
+		//
+		// GET: /Privacy/
+		public ActionResult Index() {
+			return View();
+		}
+
+	}
+}
