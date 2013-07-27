81e672144c9f7e0d8231bf72d47836bdac5eb3a7
Andrew Arnott <andrewarnott@gmail.com>
2013-07-27 15:24:45 -0700

    Adds IronPigeon.WinPhone8 project and updates .nuspec files to support Windows Phone 8 projects.

---
+++ src/IronPigeon.WinPhone8/WinPhoneChannel.cs
@@ -0,0 +1,13 @@
+﻿namespace IronPigeon.WinPhone8 {
+	using System;
+	using System.Collections.Generic;
+	using System.Composition;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	[Export(typeof(Channel))]
+	[Shared]
+	public class WinPhoneChannel : Channel {
+	}
+}
