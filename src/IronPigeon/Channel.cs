350cfd1ff53e09f69bb8e23805bdfc3fdff73292
Andrew Arnott <andrewarnott@gmail.com>
2013-10-23 22:46:13 -0700

    Fixes NullReferenceExceptions that were introduced with the recent debugging help checkin.

--- src/IronPigeon/Channel.cs
+++ src/IronPigeon/Channel.cs
@@ -413,7 +413,11 @@
 			postContent.Position = 0;
 
 			using (var response = await this.HttpClient.PostAsync(builder.Uri, new StreamContent(postContent), cancellationToken)) {
-				string responseContent = await response.Content.ReadAsStringAsync();
+				if (response.Content != null) {
+					// Just to help in debugging.
+					string responseContent = await response.Content.ReadAsStringAsync();
+				}
+
 				response.EnsureSuccessStatusCode();
 				var receipt = new NotificationPostedReceipt(recipient, response.Headers.Date);
 				return receipt;
