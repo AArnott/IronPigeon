735313eaeec6805e193976c5b921000bed9ecc2d
Andrew Arnott <andrewarnott@gmail.com>
2013-12-14 22:23:38 -0800

    Fixes build breaks in Xamarin projects.

--- src/IronPigeon.MonoAndroid/Providers/AndroidCryptoProvider.cs
+++ src/IronPigeon.MonoAndroid/Providers/AndroidCryptoProvider.cs
@@ -154,5 +154,20 @@ namespace IronPigeon.MonoAndroid.Providers {
 		public override void GenerateEncryptionKeyPair(out byte[] keyPair, out byte[] publicKey) {
 			throw new NotImplementedException();
 		}
+
+		/// <inheritdoc/>
+		public override void BeginNegotiateSharedSecret(out byte[] privateKey, out byte[] publicKey) {
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc/>
+		public override void RespondNegotiateSharedSecret(byte[] remotePublicKey, out byte[] ownPublicKey, out byte[] sharedSecret) {
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc/>
+		public override void EndNegotiateSharedSecret(byte[] ownPrivateKey, byte[] remotePublicKey, out byte[] sharedSecret) {
+			throw new NotImplementedException();
+		}
 	}
 }
