d07efa979ba4c28be2213b14cb72e415c1532bc8
Andrew Arnott <andrewarnott@gmail.com>
2014-04-19 16:45:58 -0700

    Removes all mobile platform projects and moved their channels into the main PCL.

--- src/IronPigeon/WinPhoneChannel.cs
+++ src/IronPigeon/WinPhoneChannel.cs
@@ -10,8 +10,6 @@
 	using System.Threading;
 	using System.Threading.Tasks;
 
-	using Microsoft.Phone.Notification;
-
 	using Validation;
 
 	/// <summary>
@@ -23,7 +21,7 @@
 		/// <summary>
 		/// Registers a Windows 8 application to receive push notifications for incoming messages.
 		/// </summary>
-		/// <param name="pushNotificationChannel">The push notification channel.</param>
+		/// <param name="pushNotificationChannelUri">The push notification channel's ChannelUri property.</param>
 		/// <param name="pushContent">Content of the push.</param>
 		/// <param name="toastLine1">The first line in the toast notification to send.</param>
 		/// <param name="toastLine2">The second line in the toast notification to send.</param>
@@ -32,13 +30,13 @@
 		/// <returns>
 		/// A task representing the async operation.
 		/// </returns>
-		public async Task RegisterPushNotificationChannelAsync(HttpNotificationChannel pushNotificationChannel, string pushContent = null, string toastLine1 = null, string toastLine2 = null, string tileTemplate = null, CancellationToken cancellationToken = default(CancellationToken)) {
-			Requires.NotNull(pushNotificationChannel, "pushNotificationChannel");
+		public async Task RegisterPushNotificationChannelAsync(Uri pushNotificationChannelUri, string pushContent = null, string toastLine1 = null, string toastLine2 = null, string tileTemplate = null, CancellationToken cancellationToken = default(CancellationToken)) {
+			Requires.NotNull(pushNotificationChannelUri, "pushNotificationChannelUri");
 
 			var request = new HttpRequestMessage(HttpMethod.Put, this.Endpoint.PublicEndpoint.MessageReceivingEndpoint);
 			request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", this.Endpoint.InboxOwnerCode);
 			request.Content = new FormUrlEncodedContent(new Dictionary<string, string> {
-				{ "wp8_channel_uri", pushNotificationChannel.ChannelUri.AbsoluteUri },
+				{ "wp8_channel_uri", pushNotificationChannelUri.AbsoluteUri },
 				{ "wp8_channel_content", pushContent ?? string.Empty },
 				{ "wp8_channel_toast_text1", toastLine1 ?? string.Empty },
 				{ "wp8_channel_toast_text2", toastLine2 ?? string.Empty },
--- src/IronPigeon/WinRTChannel.cs
+++ src/IronPigeon/WinRTChannel.cs
@@ -10,7 +10,6 @@
 	using System.Threading;
 	using System.Threading.Tasks;
 	using Validation;
-	using Windows.Networking.PushNotifications;
 
 	/// <summary>
 	/// A channel for sending or receiving secure messages with additional WinRT specific functionality.
@@ -29,21 +28,22 @@
 		/// <summary>
 		/// Registers a Windows 8 application to receive push notifications for incoming messages.
 		/// </summary>
-		/// <param name="pushNotificationChannel">The push notification channel.</param>
+		/// <param name="pushNotificationChannelUri">The push notification channel.</param>
+		/// <param name="channelExpiration">When the channel will expire.</param>
 		/// <param name="pushContent">Content of the push.</param>
 		/// <param name="cancellationToken">The cancellation token.</param>
 		/// <returns>A task representing the async operation.</returns>
-		public async Task RegisterPushNotificationChannelAsync(PushNotificationChannel pushNotificationChannel, string pushContent, CancellationToken cancellationToken = default(CancellationToken)) {
-			Requires.NotNull(pushNotificationChannel, "pushNotificationChannel");
+		public async Task RegisterPushNotificationChannelAsync(Uri pushNotificationChannelUri, DateTime channelExpiration, string pushContent, CancellationToken cancellationToken = default(CancellationToken)) {
+			Requires.NotNull(pushNotificationChannelUri, "pushNotificationChannelUri");
 			Requires.ValidState(!string.IsNullOrEmpty(this.PackageSecurityIdentifier), "PackageSecurityIdentifier must be initialized first.");
 
 			var request = new HttpRequestMessage(HttpMethod.Put, this.Endpoint.PublicEndpoint.MessageReceivingEndpoint);
 			request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", this.Endpoint.InboxOwnerCode);
 			request.Content = new FormUrlEncodedContent(new Dictionary<string, string> {
 				{ "package_security_identifier", this.PackageSecurityIdentifier },
-				{ "channel_uri", pushNotificationChannel.Uri },
+				{ "channel_uri", pushNotificationChannelUri.AbsoluteUri },
 				{ "channel_content", pushContent ?? string.Empty },
-				{ "expiration", pushNotificationChannel.ExpirationTime.ToString(CultureInfo.InvariantCulture) },
+				{ "expiration", channelExpiration.ToString(CultureInfo.InvariantCulture) },
 			});
 			var response = await this.HttpClient.SendAsync(request, cancellationToken);
 			response.EnsureSuccessStatusCode();
