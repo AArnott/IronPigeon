503e1e44dd6fcbf6b6d47aaba4e2a36c8ab259ba
Andrew Arnott <andrewarnott@gmail.com>
2020-12-22 17:55:47 -0700

    Fix build warnings

--- src/WpfChatroom/EndpointAndAddressBookUri.cs
+++ src/WpfChatroom/EndpointAndAddressBookUri.cs
@@ -6,18 +6,32 @@ namespace IronPigeon
     using System;
     using System.Runtime.Serialization;
 
+    /// <summary>
+    /// A tuple containing an endpoint's private data and a location where the public endpoint can be downloaded.
+    /// </summary>
     [DataContract]
     public class EndpointAndAddressBookUri
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EndpointAndAddressBookUri"/> class.
+        /// </summary>
+        /// <param name="addressBookUri">The URI where the address book entry is stored.</param>
+        /// <param name="endpoint">The user's own endpoint.</param>
         public EndpointAndAddressBookUri(Uri addressBookUri, OwnEndpoint endpoint)
         {
             this.AddressBookUri = addressBookUri;
             this.Endpoint = endpoint;
         }
 
+        /// <summary>
+        /// Gets the URI where the address book entry is stored.
+        /// </summary>
         [DataMember]
         public Uri AddressBookUri { get; }
 
+        /// <summary>
+        /// Gets the user's own endpoint.
+        /// </summary>
         [DataMember]
         public OwnEndpoint Endpoint { get; }
     }
